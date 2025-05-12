ae486f7a8f31999e123cedf5af2d9aeec67f1ba5
Elmostcher1 <ahmedmohamed200571@gmail.com>
2025-05-12 12:08:11 +0300

    finally

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Pr_react/' ,
+  base: '/project_react/' ,
   plugins: [react()],
 });
