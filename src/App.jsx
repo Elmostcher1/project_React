bc124427f1738b0956bd321630c78387881e2418
Elmostcher1 <ahmedmohamed200571@gmail.com>
2025-05-17 00:07:26 +0300

    Lotie

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import React  , {use, useEffect, useState} from 'react';
+import React  , {Suspense , lazy}  from 'react';
 import {createBrowserRouter,createHashRouter,RouterProvider} from 'react-router-dom';
 import toast , {Toaster} from "react-hot-toast"
 import Swal from 'sweetalert2'
 // links To Components---
-import Layout from './components/Layout/Layout';
-import Home from './components/Home/Home';
-import Contact from './components/Contact/Contact';
+const  Layout =  lazy( ()=> import ('./components/Layout/Layout'))
+const  Home = lazy(()=>import ( './components/Home/Home'))
+const  Contact = lazy(()=>import ( './components/Contact/Contact'))
+const  Shop = lazy(()=>import ( './components/Shop/Shop'))
+const  Cart = lazy(()=>import ( './components/Cart/Cart'))
+const About  = lazy(()=>import( './components/About/About'))
+ const SingleProducts = lazy(()=> import ( './components/Shop/SingleProducts/SingleProducts'))
+ const  Wishlist = lazy(()=> import ( './components/wishlist/wishlist'))
 import Child1 from './components/Home/Featured/Child1/Child1';
 import Child2 from './components/Home/Featured/Child2/Child2';
 import Child3 from './components/Home/Featured/Child3/Child3';
-import About from './components/About/About';
-import Shop from './components/Shop/Shop';
-import Cart from './components/Cart/Cart';
-import SingleProducts from './components/Shop/SingleProducts/SingleProducts';
 import Nested1 from './components/Shop/SingleProducts/nested_1/nested_1';
 import Nested2 from './components/Shop/SingleProducts/nested_2/nested_2';
 import Nested3 from './components/Shop/SingleProducts/nested_3/nested_3';
-import Wishlist from './components/wishlist/wishlist';
+import LotieHandel from './LotieHandel';
+
+
 
 function App() {
 
@@ -27,32 +30,34 @@ function App() {
 const router = createHashRouter([
   {
     path : '' ,
-    element : <Layout /> ,
+    element : <Suspense fallback={<LotieHandel status={'main_pages'}/>}><Layout /> </Suspense> ,
     children : [
-      {index : '' , element: <Home /> , children:[
+      {index : '' , element: <Suspense fallback={<LotieHandel status={'netsed_page'} />}><Home /></Suspense> , children:[
         {path : '' , element : <Child1   />} ,
         {path : 'Best' , element : <Child2   />} ,
         {path : 'featured' , element : <Child3   />} ,
       ] },
       
-      {path : 'contact' , element: <Contact />} ,
+      {path : 'contact' , element: <Suspense fallback={<LotieHandel status={'netsed_page'} />}><Contact /></Suspense>} ,
       
-      {path : 'about' , element: <About />} ,
+      {path : 'about' , element: <Suspense fallback={<LotieHandel status={'netsed_page'} />}><About /></Suspense>} ,
 
-      {path : 'cart' , element: <Cart />} ,
+      {path : 'cart' , element: <Suspense fallback={<LotieHandel status={'netsed_page'} />}><Cart /></Suspense>} ,
 
       
-      {path : 'shop' , element: <Shop />} ,
-      {path : 'wishlist' , element: <Wishlist />} ,
+      {path : 'shop' , element: <Suspense fallback={<LotieHandel status={'netsed_page'} />}><Shop /></Suspense>} ,
+      {path : 'wishlist' , element: <Suspense fallback={<LotieHandel status={'netsed_page'} />} > <Wishlist /></Suspense>} ,
 
 
-      {path : '/singlepro/:id' , element: <SingleProducts /> , children : [
+      {path : '/singlepro/:id' , element: <Suspense fallback={<LotieHandel status={'netsed_page'} />}><SingleProducts /></Suspense> , children : [
          {path : '' , element: <Nested1  />} ,
          {path : 'nested_2' , element: <Nested2  />} ,
          {path : 'nested_3' , element: <Nested3  />} ,     
        ]} ,
 
-    ]}
+    ] ,
+    errorElement : <LotieHandel status={"error"} />
+  }
 ])
 
 
---
+++ src/LotieHandel.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Lottie from "lottie-react";
+import loder from '../public/loties/loderreactBlack.json'
+import loder_2 from '../public/loties/loder_nested_page.json'
+import error from '../public/loties/error_react_black.json'
+
+
+function LotieHandel({status}) {
+  return (
+      <div className='vh-100 flex'>
+        <div className='flex flex-column w-50'>
+        {status === 'main_pages' ? 
+            <Lottie animationData={loder_2} />
+            : status === 'netsed_page' ?
+            <Lottie animationData={loder} />
+            : status === 'error' &&
+            <Lottie animationData={error} />
+        }
+        </div>
+    </div>
+  )
+}
+
+export default LotieHandel
